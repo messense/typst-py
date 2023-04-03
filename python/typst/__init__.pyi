71a813551f5bd2117d22298d5918188a48957b6d
messense <messense@icloud.com>
2023-04-03 11:53:14 +0800

    Add typing

---
+++ python/typst/__init__.pyi
@@ -0,0 +1,15 @@
+import pathlib
+from typing import List, Optional, TypeVar
+
+PathLike = TypeVar("PathLike", str, pathlib.Path)
+
+class Compiler:
+    def __init__(self, root: PathLike, font_paths: List[PathLike] = []) -> None: ...
+    def compile(self, input: PathLike, output: Optional[PathLike] = None) -> None: ...
+
+def compile(
+    input: PathLike,
+    output: Optional[PathLike] = None,
+    root: Optional[PathLike] = None,
+    font_paths: List[PathLike] = [],
+) -> None: ...
