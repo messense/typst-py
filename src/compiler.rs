8bb3bf28675ea3807c93f8df9baa3382fab9d60e
messense <messense@icloud.com>
2023-04-26 22:28:37 +0800

    Update typst to 0.3.0

--- src/compiler.rs
+++ src/compiler.rs
@@ -9,7 +9,7 @@ use elsa::FrozenVec;
 use memmap2::Mmap;
 use once_cell::unsync::OnceCell;
 use same_file::Handle;
-use siphasher::sip128::{Hasher128, SipHasher};
+use siphasher::sip128::{Hasher128, SipHasher13};
 use typst::diag::{FileError, FileResult, StrResult};
 use typst::eval::Library;
 use typst::font::{Font, FontBook, FontInfo};
@@ -191,7 +191,7 @@ impl PathHash {
     fn new(path: &Path) -> FileResult<Self> {
         let f = |e| FileError::from_io(e, path);
         let handle = Handle::from_path(path).map_err(f)?;
-        let mut state = SipHasher::new();
+        let mut state = SipHasher13::new();
         handle.hash(&mut state);
         Ok(Self(state.finish128().as_u128()))
     }
