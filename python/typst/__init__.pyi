ad84991ddcae6cf4cb856e1517e3f40584e40bd6
Jocelyn Gas <jocelyn.gas@dillygence.com>
2023-09-05 13:51:45 +0200

    feat: add overload to compile

--- python/typst/__init__.pyi
+++ python/typst/__init__.pyi
@@ -1,11 +1,28 @@
 import pathlib
-from typing import List, Optional, TypeVar
+from typing import List, Optional, TypeVar, overload
 
 PathLike = TypeVar("PathLike", str, pathlib.Path)
+
+@overload
+def compile(
+    input: PathLike,
+    output: PathLike,
+    root: Optional[PathLike] = None,
+    font_paths: Optional[List[PathLike]] = None,
+) -> None: ...
+
+@overload
+def compile(
+    input: PathLike,
+    output: None = None,
+    root: Optional[PathLike] = None,
+    font_paths: Optional[List[PathLike]] = None,
+) -> bytes: ...
+
 
 def compile(
     input: PathLike,
     output: Optional[PathLike] = None,
     root: Optional[PathLike] = None,
-    font_paths: List[PathLike] = [],
+    font_paths: Optional[List[PathLike]] = None,
 ) -> Optional[bytes]: ...
