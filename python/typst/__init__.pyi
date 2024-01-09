6fc9ef1955c32ca6c79de25933885ca688694846
OrangeX4 <318483724@qq.com>
2024-01-09 13:43:43 +0800

    feat: bring back Compiler to avoid reinitialization

--- python/typst/__init__.pyi
+++ python/typst/__init__.pyi
@@ -3,6 +3,36 @@ from typing import List, Optional, TypeVar, overload
 
 PathLike = TypeVar("PathLike", str, pathlib.Path)
 
+class Compiler:
+    def __init__(
+        self,
+        input: PathLike,
+        root: Optional[PathLike] = None,
+        font_paths: List[PathLike] = [],
+    ) -> None:
+        """Initialize a Typst compiler.
+        Args:
+            input (PathLike): Project's main .typ file.
+            root (Optional[PathLike], optional): Root path for the Typst project.
+            font_paths (List[PathLike]): Folders with fonts.
+        """
+    def compile(
+        self,
+        output: Optional[PathLike] = None,
+        format: Optional[str] = None,
+        ppi: Optional[float] = None,
+    ) -> Optional[bytes]:
+        """Compile a Typst project.
+        Args:
+            output (Optional[PathLike], optional): Path to save the compiled file.
+            Allowed extensions are `.pdf`, `.svg` and `.png`
+            format (Optional[str]): Output format.
+            Allowed values are `pdf`, `svg` and `png`.
+            ppi (Optional[float]): Pixels per inch for PNG output, defaults to 144.
+        Returns:
+            Optional[bytes]: Return the compiled file as `bytes` if output is `None`.
+        """
+
 @overload
 def compile(
     input: PathLike,
