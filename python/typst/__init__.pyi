61d47f854df6bed680a445dd35910c10031a6044
OrangeX4 <34951714+OrangeX4@users.noreply.github.com>
2024-05-21 12:39:53 +0800

    feat: export multiple pages for png and svg (#34)
    * update to typst 0.11.1
    
    * feat: export multiple pages for png and svg
    
    * docs: update type hint
    
    * refactor: remove ImageBuffer
    
    * docs: update README

--- python/typst/__init__.pyi
+++ python/typst/__init__.pyi
@@ -1,5 +1,5 @@
 import pathlib
-from typing import List, Optional, TypeVar, overload, Dict
+from typing import List, Optional, TypeVar, overload, Dict, Union
 
 PathLike = TypeVar("PathLike", str, pathlib.Path)
 
@@ -9,7 +9,7 @@ class Compiler:
         input: PathLike,
         root: Optional[PathLike] = None,
         font_paths: List[PathLike] = [],
-        sys_inputs: Dict[str, str] = {}
+        sys_inputs: Dict[str, str] = {},
     ) -> None:
         """Initialize a Typst compiler.
         Args:
@@ -18,12 +18,13 @@ class Compiler:
             font_paths (List[PathLike]): Folders with fonts.
             sys_inputs (Dict[str, str]): string key-value pairs to be passed to the document via sys.inputs
         """
+
     def compile(
         self,
         output: Optional[PathLike] = None,
         format: Optional[str] = None,
         ppi: Optional[float] = None,
-    ) -> Optional[bytes]:
+    ) -> Optional[Union[bytes, List[bytes]]]:
         """Compile a Typst project.
         Args:
             output (Optional[PathLike], optional): Path to save the compiled file.
@@ -32,7 +33,7 @@ class Compiler:
             Allowed values are `pdf`, `svg` and `png`.
             ppi (Optional[float]): Pixels per inch for PNG output, defaults to 144.
         Returns:
-            Optional[bytes]: Return the compiled file as `bytes` if output is `None`.
+            Optional[Union[bytes, List[bytes]]]: Return the compiled file as `bytes` if output is `None`.
         """
 
 @overload
@@ -43,7 +44,7 @@ def compile(
     font_paths: List[PathLike] = [],
     format: Optional[str] = None,
     ppi: Optional[float] = None,
-    sys_inputs: Dict[str, str] = {}
+    sys_inputs: Dict[str, str] = {},
 ) -> None: ...
 @overload
 def compile(
@@ -53,7 +54,7 @@ def compile(
     font_paths: List[PathLike] = [],
     format: Optional[str] = None,
     ppi: Optional[float] = None,
-    sys_inputs: Dict[str, str] = {}
+    sys_inputs: Dict[str, str] = {},
 ) -> bytes: ...
 def compile(
     input: PathLike,
@@ -62,8 +63,8 @@ def compile(
     font_paths: List[PathLike] = [],
     format: Optional[str] = None,
     ppi: Optional[float] = None,
-    sys_inputs: Dict[str, str] = {}
-) -> Optional[bytes]:
+    sys_inputs: Dict[str, str] = {},
+) -> Optional[Union[bytes, List[bytes]]]:
     """Compile a Typst project.
     Args:
         input (PathLike): Project's main .typ file.
@@ -76,5 +77,5 @@ def compile(
         ppi (Optional[float]): Pixels per inch for PNG output, defaults to 144.
         sys_inputs (Dict[str, str]): string key-value pairs to be passed to the document via sys.inputs
     Returns:
-        Optional[bytes]: Return the compiled file as `bytes` if output is `None`.
+        Optional[Union[bytes, List[bytes]]]: Return the compiled file as `bytes` if output is `None`.
     """
